42e791f5558cd8ea3f81710b2a1164aaa51714bf
Ovidiu Mura <ovidiu.mura@yahoo.com>
2026-09-02 11:32:21 -0700

    adding solution for subarray_sum function

---
+++ section18/code.py
@@ -0,0 +1,44 @@
+
+def subarray_sum(nums, target):
+    prefix_sums = {0: -1}
+    s = 0
+    for i, v in enumerate(nums):
+        s += v
+        q = s - target
+        print(q, "=", prefix_sums)
+        if q in prefix_sums.keys():
+            print(q, ":", prefix_sums[q])
+            return [prefix_sums[q]+1, i]
+        if s not in prefix_sums.keys():
+            prefix_sums[s] = i
+    return []
+
+
+nums = [1, 2, 3, 4, 5]
+target = 9
+print ( subarray_sum(nums, target) )
+
+# nums = [-1, 2, 3, -4, 5]
+# target = 0
+# print ( subarray_sum(nums, target) )
+
+# nums = [2, 3, 4, 5, 6]
+# target = 3
+# print ( subarray_sum(nums, target) )
+
+# nums = []
+# target = 0
+# print ( subarray_sum(nums, target) )
+
+
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    [1, 3]
+    [0, 3]
+    [1, 1]
+    []
+
+"""
+
