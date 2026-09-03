270819b9c8d3600aa5317fb449a36f02c005d885
Ovidiu Mura <ovidiu.mura@yahoo.com>
2026-09-03 14:34:34 -0700

    fixing indentation

--- section25-RecursiveBST/resources/SOLUTION-BST-Delete.py
+++ section25-RecursiveBST/resources/SOLUTION-BST-Delete.py
@@ -79,24 +79,24 @@ class BinarySearchTree:
         return current_node.value 
 
     def __delete_node(self, current_node, value):
-	    if current_node == None: 
-		    return None
-	    if value < current_node.value:
-		    current_node.left = self.__delete_node(current_node.left, value)
-	    elif value > current_node.value: 
-		    current_node.right = self.__delete_node(current_node.right, value)
-	    else:
-		    if current_node.left == None and current_node.right == None:
-			    return None
-		    elif current_node.left == None:
-			    current_node = current_node.right
-		    elif current_node.right == None:
-			    current_node = current_node.left
-		    else:
-			    sub_tree_min = self.min_value(current_node.right)
-			    current_node.value = sub_tree_min
-			    current_node.right = self.__delete_node(current_node.right, sub_tree_min)
-	    return current_node
+        if current_node == None: 
+             return None
+        if value < current_node.value:
+             current_node.left = self.__delete_node(current_node.left, value)
+        elif value > current_node.value: 
+            current_node.right = self.__delete_node(current_node.right, value)
+        else:
+            if current_node.left == None and current_node.right == None:
+                return None
+            elif current_node.left == None:
+                current_node = current_node.right
+            elif current_node.right == None:
+                current_node = current_node.left
+            else:
+                sub_tree_min = self.min_value(current_node.right)
+                current_node.value = sub_tree_min
+                current_node.right = self.__delete_node(current_node.right, sub_tree_min)
+        return current_node
     
     def delete_node(self, value):
         self.root = self.__delete_node(self.root, value)
