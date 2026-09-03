1058a823148593ea4b5bc506b6e67dcd52e7bed9
Ovidiu Mura <ovidiu.mura@yahoo.com>
2026-09-03 11:22:00 -0700

    adding solution for MinHeap

---
+++ section22-HeapInsert/minHeapInsert.py
@@ -0,0 +1,55 @@
+class MinHeap:
+    def __init__(self):
+        self.heap = []
+
+    def _left_child(self, index):
+        return 2 * index + 1
+
+    def _right_child(self, index):
+        return 2 * index + 2
+
+    def _parent(self, index):
+        return (index - 1) // 2
+
+    def _swap(self, index1, index2):
+        self.heap[index1], self.heap[index2] = self.heap[index2], self.heap[index1]
+
+    def insert(self, value):
+        self.heap.append(value)
+        idx = len(self.heap) - 1
+        while idx > 0:
+            parent = self._parent(idx)
+            if self.heap[idx] >= self.heap[parent]:
+                break
+            self._swap(idx, parent)
+            idx = parent
+                
+        
+
+ 
+ 
+myheap = MinHeap()
+myheap.insert(12)
+myheap.insert(10)
+myheap.insert(8)
+myheap.insert(6)
+
+print(myheap.heap)  # [6, 8, 10, 12]
+
+myheap.insert(4)
+
+print(myheap.heap)  # [4, 6, 10, 12, 8]
+
+myheap.insert(2)
+
+print(myheap.heap)  # [2, 6, 4, 12, 8, 10]
+
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    [6, 8, 10, 12]
+    [4, 6, 10, 12, 8]
+    [2, 6, 4, 12, 8, 10]
+
+"""
