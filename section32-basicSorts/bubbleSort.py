8c93c959b5cfe7d67e885b23ae77ab53cd6990a4
Ovidiu Mura <ovidiu.mura@yahoo.com>
2026-09-05 12:01:00 -0700

    adding solution for bubble sort implementation

---
+++ section32-basicSorts/bubbleSort.py
@@ -0,0 +1,21 @@
+def bubble_sort(arr):
+    for i in range(len(arr)-1, 0, -1):
+        for j in range(0, i):
+            if arr[j] > arr[j+1]:
+                t = arr[j]
+                arr[j] = arr[j+1]
+                arr[j+1] = t
+    return arr
+
+
+
+print(bubble_sort([4,2,6,5,1,3]))
+
+ 
+ 
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    [1, 2, 3, 4, 5, 6]
+    
+ """
