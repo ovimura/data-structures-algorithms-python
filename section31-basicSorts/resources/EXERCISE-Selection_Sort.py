19f15f0850228a724459b65355cf2a97a6c83b3a
Ovidiu Mura <ovidiu.mura@yahoo.com>
2026-09-05 11:40:45 -0700

    adding solution for selection sort

--- section31-basicSorts/resources/EXERCISE-Selection_Sort.py
+++ section31-basicSorts/resources/EXERCISE-Selection_Sort.py
@@ -1,12 +1,14 @@
-## WRITE SELECTION_SORT FUNCTION HERE ##
-#                                      #
-#                                      #
-#                                      #
-#                                      #
-######################################## 
-    
-
-
+def selection_sort(arr):
+    for i in range(len(arr)-1):
+        min_idx = i
+        for j in range(i+1, len(arr)):
+            if arr[j] < min_idx:
+                min_idx = j
+        if i != min_idx:
+            t = arr[j]
+            arr[i] = arr[min_idx]
+            arr[min_idx] = t
+    return arr
 
 
 print(selection_sort([4,2,6,5,1,3]))
