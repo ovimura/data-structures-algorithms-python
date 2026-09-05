5acb168d0a54b155a0b7e7444111d44396d35d7e
Ovidiu Mura <ovidiu.mura@yahoo.com>
2026-09-05 11:24:04 -0700

    implement function for bubble sort

--- section31-basicSorts/resources/EXERCISE-Bubble_Sort.py
+++ section31-basicSorts/resources/EXERCISE-Bubble_Sort.py
@@ -1,14 +1,12 @@
-## WRITE BUBBLE_SORT FUNCTION HERE ##
-#                                   #
-#                                   #
-#                                   #
-#                                   #
-##################################### 
+def bubble_sort(arr):
+    for i in range(len(arr)-1, 0, -1):
+        for j in range(i):
+            if arr[j] > arr[j+1]:
+                t = arr[j]
+                arr[j] = arr[j+1]
+                arr[j+1] = t
+    return arr
     
-
-
-
-
 
 print(bubble_sort([4,2,6,5,1,3]))
 
